5bbf7eac8d5ae7381cf4b3ed2318380fc3152863
simonhaenisch <simonhaenisch@users.noreply.github.com>
2018-10-31 16:35:16 +1300

    use strings for ids instead of numbers

--- src/interfaces/list-card-item.ts
+++ src/interfaces/list-card-item.ts
@@ -1,5 +1,5 @@
 export interface ListItem {
-	id: number;
+	id: string;
 	title: string;
 	content: JSX.Element;
 }
--- src/services/data.tsx
+++ src/services/data.tsx
@@ -1,47 +1,53 @@
 import { ListItem } from "../interfaces/list-card-item";
 
-export const getList = () =>
-	[
-		{
-			title: "First Item",
-			content: <i>Italic</i>
-		},
-		{
-			title: "Foo",
-			content: <strong>Bold</strong>
-		},
-		{
-			title: "Bar",
-			content: (
-				<strong>
-					<i>Bold Italic</i>
-				</strong>
-			)
-		},
-		{
-			title: "Foobar",
-			content: <ion-text text-uppercase>Uppercase</ion-text>
-		},
-		{
-			title: "Barfoo",
-			content: <ion-text text-center>Center</ion-text>
-		},
-		{
-			title: "Text no-wrap",
-			content: (
-				<ion-text text-nowrap>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-					quisquam error at laboriosam amet harum illo
-				</ion-text>
-			)
-		},
-		{
-			title: "Text wrap",
-			content: (
-				<ion-text text-wrap>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-					quisquam error at laboriosam amet harum illo
-				</ion-text>
-			)
-		}
-	].map<ListItem>((el, id) => ({ ...el, id }));
+export const getList = (): ListItem[] => [
+	{
+		id: "Oh77czV9",
+		title: "First Item",
+		content: <i>Italic</i>
+	},
+	{
+		id: "ea3hrCHN",
+		title: "Foo",
+		content: <strong>Bold</strong>
+	},
+	{
+		id: "UrSSYXsU",
+		title: "Bar",
+		content: (
+			<strong>
+				<i>Bold Italic</i>
+			</strong>
+		)
+	},
+	{
+		id: "IbsFudk8",
+		title: "Foobar",
+		content: <ion-text text-uppercase>Uppercase</ion-text>
+	},
+	{
+		id: "LvaKZ8xb",
+		title: "Barfoo",
+		content: <ion-text text-center>Center</ion-text>
+	},
+	{
+		id: "0tTwJ5EI",
+		title: "Text no-wrap",
+		content: (
+			<ion-text text-nowrap>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+				quisquam error at laboriosam amet harum illo
+			</ion-text>
+		)
+	},
+	{
+		id: "7XARlu7I",
+		title: "Text wrap",
+		content: (
+			<ion-text text-wrap>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+				quisquam error at laboriosam amet harum illo
+			</ion-text>
+		)
+	}
+];
